db15c18736551f818a8a90e34e1e8b3ec065f3b9
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-06-21 17:29:27 +0300

    added chart

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 let buttn= document.getElementById ('imgsresult');
+let resulet= document.getElementById ('report');
+// let chart= document.getElementById ('myChart');
 let imgArray = [
   'bag.jpg',
   'banana.jpg',
@@ -21,6 +23,7 @@ let imgArray = [
   'usb.gif',
   'water-can.jpg',
   'wine-glass.jpg'];
+  let newarry=[22,21,20]
 // let clicks = 0;
 let counter = 0;
 let imageSection = document.getElementById('imageSection');
@@ -41,24 +44,41 @@ function Imgs(name, src) {
 
 }
 Imgs.all = [];
-
 for (let i = 0; i < imgArray.length; i++) {
   new Imgs(imgArray[i].split('.')[0], imgArray[i]);
 
 
 }
+// function render() {
+//   leftindex = randomNumber(0, imgArray.length - 1);
+
+
+//   do {
+//     midindex = randomNumber(0, imgArray.length - 1);
+//     rightindex = randomNumber(0, imgArray.length - 1);
+
+
+
+//   } while (leftindex  === midindex || leftindex === rightindex || midindex === rightindex);
 function render() {
-  leftindex = randomNumber(0, imgArray.length - 1);
+  
+do { leftindex = randomNumber(0, imgArray.length - 1);
 
-
+} while(leftindex === newarry[0]||leftindex === newarry[1]||leftindex === newarry[2])
+  
+ 
   do {
-    midindex = randomNumber(0, imgArray.length - 1)
+    midindex = randomNumber(0, imgArray.length - 1);
     rightindex = randomNumber(0, imgArray.length - 1);
 
 
 
-  } while (leftindex === midindex || leftindex === rightindex || midindex === rightindex);
+  } while (leftindex  === midindex || leftindex === rightindex || midindex === rightindex || midindex === newarry[0]||midindex === newarry[1]||midindex === newarry[2]|| midindex === rightindex || rightindex === newarry[0]||rightindex === newarry[1]||rightindex === newarry[2]);
 
+newarry[0]=leftindex;
+newarry[2]=midindex;
+newarry[1]=rightindex;
+console.log(newarry);
 
   rightImage.src = Imgs.all[rightindex].src;
   midImage.src = Imgs.all[midindex].src;
@@ -68,15 +88,20 @@ function render() {
   Imgs.all[leftindex].views++;
   Imgs.all[midindex].views++;
   Imgs.all[rightindex].views++;
-//   console.log(Imgs.all);
-
+  //   console.log(Imgs.all);
 }
 
 
 
+buttn.addEventListener('click', renderbuttn);
+
+
+imageSection.addEventListener('click', eventhandler);
+render();
+
 //
 function eventhandler(e) {
-  if (counter<24){
+  if (counter<25){
     if (e.target.id === 'rightImage') {
       Imgs.all[rightindex].clicks++;
     }
@@ -88,43 +113,71 @@ function eventhandler(e) {
       Imgs.all[midindex].clicks++;
     }
   }
-  else{
+  else{counter<25
     return;
   }
+
   render();
-  counter++;
+  counter++;  
 }
 
 
 function renderbuttn (){
 
   let report = document.createElement('h2');
-  buttn.appendChild(report);
+  resulet.appendChild(report);
   report.textContent = 'report';
 
   let ul = document.createElement('ul');
-  buttn.appendChild(ul);
+  resulet.appendChild(ul);
   for (let i = 0; i <imgArray.length; i++) {
     let li = document.createElement('li');
     ul.appendChild(li);
     li.textContent = `${imgArray[i].split('.')[0]} had  ${Imgs.all[i].clicks} votes was seen  ${Imgs.all[i].views} times`;
- 
   }
+  drawChart();
+  
+    buttn.removeEventListener('click', renderbuttn);
+  
+}
+
+function drawChart() {
+
+  let name = [];
+  let views = [];
+  let clicks= [];
+
+  for(let i = 0; i < Imgs.all.length; i++) {
+    name.push(Imgs.all[i].name);
+    views.push(Imgs.all[i].views);
+    clicks.push(Imgs.all[i].clicks);
+  }
+
+  let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
+
+  let myChart = new Chart( ctx, {
+    type: 'bar',
+    data: {labels: name, datasets: [{label: '# of Votes' , data: clicks , backgroundColor: 'rgba(255, 99, 132, 0.2)',borderColor: [
+      'rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)'],borderWidth: 1 }] }, options: {scales: {y: { beginAtZero: true}}} 
+  } );
+ 
+  function addData(chart, label, color, data) {
+		chart.data.datasets.push({
+	    label: label,
+      backgroundColor: color,
+      data: data
+    });
+    
+
+    chart.update();
+}
+setTimeout(function() {
+  addData(myChart, '# of views', '#ff0000', views);
+},);
+
 
 }
 
-
-
-
-buttn.addEventListener("click", renderbuttn);
-
-
-
-imageSection.addEventListener('click', eventhandler);
-render();
-
-
-renderbuttn ();
 
 
 
@@ -152,7 +205,4 @@ function randomNumber(min, max) {
 //   }
 
 //   else if ((e.target.id === 'midImage') && counter < 25) {
-//     Imgs.all[midindex].clicks++ ;
-
-
-//   }
+//     Imgs.all[midindex].clicks++ ;}
